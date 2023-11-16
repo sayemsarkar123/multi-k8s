1c123741b9d2ff6dee251c228971f13a2555b85e
sayemsarkar123 <sayemsarkar123@gmail.com>
2023-11-16 18:57:28 +0600

    update conf

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,14 @@ docker build -t sayemsarkar123/multi-worker:latest -t sayemsarkar123/multi-worke
 
 docker push sayemsarkar123/multi-client:latest
 docker push sayemsarkar123/multi-server:latest
-docker push sayemsarakr123/multi-worker:latest
+docker push sayemsarkar123/multi-worker:latest
 
 docker push sayemsarkar123/multi-client:$GIT_SHA
 docker push sayemsarkar123/multi-server:$GIT_SHA
-docker push sayemsarakr123/multi-worker:$GIT_SHA
+docker push sayemsarkar123/multi-worker:$GIT_SHA
 
 kubectl apply -f k8s
 
 kubectl set image deployment/client-deployment client=sayemsarkar123/multi-client:$GIT_SHA
-kubectl set image deployment/server-deployment server=sayemsarakr123/multi-server:$GIT_SHA
+kubectl set image deployment/server-deployment server=sayemsarkar123/multi-server:$GIT_SHA
 kubectl set image deployment/worker-deployment worker=sayemsarkar123/multi-worker:$GIT_SHA
